d8ed540a6b44b5be135b31ee17d6705697515c9b
Christos <christos.polymeropoulos@gmail.com>
2015-12-10 16:46:58 -0500

    User for and profile pages

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,15 +3,14 @@ class User < ActiveRecord::Base
 before_validation :prep_email
 before_save :create_avatar_url
 
-attr_accessible :avatar_url, :email, :name, :password, :password_confirmation, :username
-
 has_secure_password
 
 validates :name, presence: true
 	
 validates :username, uniqueness: true, presence: true
 
-validates :email, uniqueness: true, presence: true, format: { with: /^[\w.+-]+@([\w]+.)+\w+$/ }
+#validates :email, uniqueness: true, presence: true, format: { with: /^[\w.+-]+@([\w]+.)+\w+$/ }
+validates :email, uniqueness: true, presence: true, format: { with: /.*[\w.+-]+@([\w]+.)+\w+\z/ }
 
 
 private
