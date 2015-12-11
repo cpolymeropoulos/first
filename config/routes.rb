39f88f2a6e1a0a0b7c947a59c5f8bcdb0a81a5da
Christos <christos.polymeropoulos@gmail.com>
2015-12-11 15:37:45 -0500

    added buddies

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,9 @@ Rails.application.routes.draw do
 
   resources :users
   root to: 'users#new'
+
+  get 'buddies', to: 'users#buddies', as: 'buddies'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
