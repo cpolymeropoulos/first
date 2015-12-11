d60595e06e775daa82e06fc711e79942e4087d92
Christos <christos.polymeropoulos@gmail.com>
2015-12-11 15:30:10 -0500

    Following and followers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,6 +18,10 @@ end
 def show
   @user = User.find(params[:id])
   @ribbit = Ribbit.new
+@relationship = Relationship.where(
+    follower_id: current_user.id,
+    followed_id: @user.id
+).first_or_initialize if current_user
 end
 
 def user_params
