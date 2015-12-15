77143c28d1cdb1710e2289ca016ecec254c28855
Christos <christos.polymeropoulos@gmail.com>
2015-12-15 11:57:14 -0500

    changed default redirecting

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,8 @@ class UsersController < ApplicationController
 
 def new
     if current_user
-        redirect_to buddies_path
+#        redirect_to buddies_path
+	redirect_to current_user
     else
         @user = User.new
     end
@@ -25,19 +26,23 @@ def create
 end
 
 def show
-  @user = User.find(params[:id])
-  @ribbit = Ribbit.new
-@relationship = Relationship.where(
-    follower_id: current_user.id,
-    followed_id: @user.id
-).first_or_initialize if current_user
+    begin
+	@user = User.find(params[:id])
+	@ribbit = Ribbit.new
+	@relationship = Relationship.where(
+	    follower_id: current_user.id,
+	    followed_id: @user.id
+	).first_or_initialize if current_user
+    rescue ActiveRecord::RecordNotFound => e
+	redirect_to users_path, notice: "No such user."
+    end
 end
 
 def buddies
     if current_user
         @ribbit = Ribbit.new
         buddies_ids = current_user.followeds.map(&:id).push(current_user.id)
-        @ribbits = Ribbit.find_all_by_user_id buddies_ids
+        @ribbits = Ribbit.where(user_id: buddies_ids)
     else
         redirect_to root_url
     end
